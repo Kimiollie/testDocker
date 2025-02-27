3b504c11a3bd05622c4516a0c154e013e80b198a
Kim Tran Do <Kim.Tran-Do@metropolia.fi>
2025-02-27 13:33:36 +0200

    update

--- src/main/java/TimeCal.java
+++ src/main/java/TimeCal.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class TimeCal {
+    public static void main(String[] args) {
+        System.out.println("Time needs " + timeCalculator(100.0, 100.0));
+    }
+
+    public static double timeCalculator(double x, double y) {
+        return x/y;
+    }
 }
